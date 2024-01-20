fdcc236c53a2ef1bdc17d727fe614149e4723b29
anagordon <143830954+anagordon@users.noreply.github.com>
2024-01-20 15:36:20 -0500

    volume slider finished

--- McGameJam2024/Assets/Ana/VolumeSaveController.cs
+++ McGameJam2024/Assets/Ana/VolumeSaveController.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class VolumeSaveController : MonoBehaviour
 {
     [SerializeField] private Slider volumeSlider = null;
 
-    [SerializeField] private TextMeshProUGUI volumeTextUI = null;
+    [SerializeField] private Text volumeTextUI = null;
     
+    private void Start()
+    {
+        LoadValues();
+    }
 
     public void VolumeSlider(float volume)
     {
-        //volumeTextUI.TextMeshProUGUI = volume.ToString("0.0");
+        volumeTextUI.text = volume.ToString("0.0");
+    }
+
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
