19811fc3d4a26843d5a364a87e8adc2f328acaef
Fabian Saldana <114820909+FabianSaldana@users.noreply.github.com>
2024-01-20 20:12:17 -0500

    Updated the companion
    Companion

--- McGameJam2024/Assets/FabianFolder/CompanionFabianFolder/CompanionFollowMe.cs
+++ McGameJam2024/Assets/FabianFolder/CompanionFabianFolder/CompanionFollowMe.cs
@@ -8,17 +8,27 @@ public class CompanionFollowMe : MonoBehaviour
     public Transform companionAnchor;
     public float time = 0.3f;
 
+    //public float FloatStrength;
+    public float RandomRotationStrength;
+
+    //public Rigidbody rb;
+
     private Vector3 velocity;
 
     void Start()
     {
         companionAnchor = GameObject.Find("companionAnchor").transform;
+        //rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position, companionAnchor.position, ref velocity, time);
+        //rb.AddForce(Vector3.up * FloatStrength);
+        transform.Rotate(RandomRotationStrength, RandomRotationStrength, RandomRotationStrength);
+
     }
+
 
 
 }
