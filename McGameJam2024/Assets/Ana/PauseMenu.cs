0065b040ecdc8c009703c8f5a7deb76860e66731
anagordon <143830954+anagordon@users.noreply.github.com>
2024-01-20 22:51:33 -0500

    bunch of stuff hehe

--- McGameJam2024/Assets/Ana/PauseMenu.cs
+++ McGameJam2024/Assets/Ana/PauseMenu.cs
@@ -1,22 +1,52 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject panel; 
+    public PlayerCam CameraController;
+    public float defaultX, defaultY;
+    public Boolean pressed;
 
-    public void Bla()
+    
+    void Start() 
     {
-        //if escape is pressed, open/close settings panel
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            panel.SetActive(true);
-        }
-        else
-        {
-            panel.SetActive(false);
-        }
+        panel.SetActive(false);
+        defaultX = CameraController.sensX;
+        defaultY = CameraController.sensY;
+        pressed = false;
     }
 
+    public void Update()
+    {
+         
+        //if escape is pressed, open/close settings panel
+        if (Input.GetKey(KeyCode.P))
+        {
+            
+            pressed = !pressed;
+            System.Threading.Thread.Sleep(200);
+            if (pressed) 
+            {
+                panel.SetActive(true);
+                CameraController.sensX = 0f;
+                CameraController.sensY = 0f;
+                Time.timeScale = 0f;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            } 
+            else 
+            {
+                panel.SetActive(false);
+                CameraController.sensX = defaultX;
+                CameraController.sensY = defaultY;
+                Time.timeScale = 1f;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
 }
